41f6d54521d5076ea9bea96ee2ee1ad982ba7f1b
AlejandroFarfan <alejandrofarfanm@gmail.com>
2019-09-20 11:05:55 -0500

    Fixed error socket hang up

--- handler.js
+++ handler.js
@@ -5,20 +5,37 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const getChrome = require("./getChrome");
 
-module.exports.takeScreenshot = async (event, context, callback) => {
-    
-  const targetUrl = event.url;
-	
-	if(( targetUrl )){
-		console.log("ok", targetUrl);
+module.exports.takeScreenshot = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+ console.log('Received event:', JSON.stringify(event, null, 2));
+  const eventurl = event.url;
+  const eventbody = event.body;
+  var urlPostApi;
+  if(eventbody){
+	  const body = JSON.parse(eventbody);
+	  urlPostApi = body.url;
+  }
+   
+   var targetUrl;
+   if(eventurl){
+	   targetUrl=eventurl;
+	}else if(urlPostApi){
+		targetUrl=urlPostApi;
 	}else{
-		console.log("Not ok");
+		return {
+			statusCode: 422,
+			body: JSON.stringify({
+			  message: 'Error: Url is not valid',
+			  url: `${targetUrl}`
+			}),
+		};	
 	}	
-  const chrome = await getChrome();
-  const browser = await puppeteer.connect({
-	  browserWSEndpoint: chrome.endpoint,
-    defaultViewport: { height: 1080, width: 1920 ,isMobile:true}
-  });
+  const chrome = await getChrome();  
+  const  browser = await puppeteer.connect({
+		  browserWSEndpoint: chrome.endpoint,
+		  defaultViewport: { height: 1080, width: 1920 ,isMobile:true},
+		  args: ['--no-sandbox', '--disable-setuid-sandbox']
+	  });
 
   try {
     const page = await browser.newPage();
@@ -26,39 +43,44 @@ module.exports.takeScreenshot = async (event, context, callback) => {
 		waitUntil: ["domcontentloaded", "networkidle2"]
 	});
 	
-	var today = new Date();
+	var today = await new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var time = today.getHours() + "" + today.getMinutes() + "" + today.getSeconds();
     var dateTime = date+'_'+time;
 	
-	await page.screenshot({path: `screenshot-${dateTime}.png`});
+	//await page.screenshot({path: `screenshot-${dateTime}.png`});
 	
 	const buffer = await page.screenshot();
-    await s3.putObject({
+	var params = {
 		Bucket: process.env.BUCKET,
 		Key: `screenshot-${dateTime}.png`,
 		Body: buffer,
 		ContentType: 'image/png',
 		ACL: 'public-read'
-	}, (err) => {
-		if(err) {
-			console.log(err);
-		}
-	});
-		
-//    }).promise();
+	};
+	var putObjectPromise = await s3.upload(params).promise();
+	var datalocation = putObjectPromise.Location;
+	console.log(`${datalocation}`);
+	await browser.disconnect();
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+		  url: `${datalocation}`
+		}),
+	};
   }catch (error) {
-	  console.log(error);
-  } finally {
-    await browser.close();
-  }
-
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  });
+	  console.warn(error);
+	  await browser.disconnect();
+	  return {
+		statusCode: 421,
+		body: JSON.stringify({
+		  message: 'Error: Url can not be rendered',
+		  error: `${error}`
+		})
+	  };	  
+   }
+   return {
+		statusCode: 210
+	}
 };
 
